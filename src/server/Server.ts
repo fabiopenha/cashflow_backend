82d7ee8e8c55533c5a648648528b1f818cfee968
Fábio Penha <fabiopenha95@outlook.com>
2022-11-22 23:11:41 -0300

    feat: code style config added

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const server = express();
 
+interface Ititle {
+	
+}
+
 server.get('/', (req, res) => {
-    return res.send("It's working!")
+	return res.send('It\'s working!');
 });
 
 export { server };
