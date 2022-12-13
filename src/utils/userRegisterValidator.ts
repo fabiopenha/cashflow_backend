aaef47e88aa2072a1b978a1b13957f564477777d
Fábio Penha <fabiopenha95@outlook.com>
2022-12-12 23:35:14 -0300

    fix: validation errors message fixed

--- src/utils/userRegisterValidator.ts
+++ src/utils/userRegisterValidator.ts
@@ -13,9 +13,15 @@ export interface IUserRegister {
 export const schema: yup.SchemaOf<IUserRegister> = yup.object().shape({
   name: yup.string().required('O nome deve ser inserido.'),
   email: yup.string().email().required('O e-mail deve ser inserido.'),
-  phone: yup.string().required('Insira um telefone válido'),
+  phone: yup.string().required('Insira um telefone válido.'),
   occupation: yup.string().required('Insira a sua profissão.'),
-  password: yup.string().min(8).max(24).required('Insira a senha correta.'),
-  passwordConfirm: yup.string().required('Por favor, confirme a sua senha')
-    .oneOf([yup.ref('password')], 'A senha é diferente.')
+  password: yup
+    .string()
+    .min(8, 'a senha deve ter no mínimo 8 caracteres.')
+    .max(24, 'a senha deve ter no máximo 24 caracteres.')
+    .required('Insira a senha correta.'),
+  passwordConfirm: yup
+    .string()
+    .required('Por favor, confirme a sua senha.')
+    .oneOf([yup.ref('password')], 'A senha é diferente.'),
 });
