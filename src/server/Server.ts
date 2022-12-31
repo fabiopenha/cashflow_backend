b6b937881ed727cf78ce7607943f2418909ce177
Fábio Penha <fabiopenha95@outlook.com>
2022-12-31 02:27:45 -0300

    feat: documenting create activity endpoint

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -4,11 +4,25 @@ import cookieParser from 'cookie-parser';
 import { DbConnect } from '../database/db';
 import { userRoutes } from '../routes/UserRoute';
 import { activityRoutes } from '../routes/ActivityRoute';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+import { dirname } from 'path';
 
 DbConnect();
 
 const server = express();
 server.use(express.json());
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+server.get('/swagger', (request, response) => {
+  return response.sendFile(process.cwd() + '/src/swagger.json');
+});
+
+server.get('/docs', (request, response) => {
+  return response.sendFile(process.cwd() + '/index.html');
+});
+
 server.use(cookieParser());
 server.use(express.static('uploads'));
 server.use(express.urlencoded({extended: true}));
